5e5ed61508bec72c80bbbbba1ee6e578ea9d8fc5
hwon22 <s2018s06@e-mirim.hs.kr>
2020-05-12 21:38:48 +0900

    삼각형으로 별 찍기 실습

--- startree/Program.cs
+++ startree/Program.cs
@@ -11,6 +11,21 @@ namespace startree
         static void Main(string[] args)
         {
 
+            int num = 8;
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = num - 1; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+
         }
     }
 }
